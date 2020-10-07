0ac306b1fc2200dc5b77240a1585b193c32a920f
Prashant060794 <prashant060794@gmail.com>
2020-10-07 13:34:01 +0530

    First commit

---
+++ src/main/java/com/arihant/bio/abesoft/AbesoftApplication.java
@@ -0,0 +1,13 @@
+package com.arihant.bio.abesoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbesoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbesoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arihant/bio/abesoft/AbesoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arihant.bio.abesoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbesoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
